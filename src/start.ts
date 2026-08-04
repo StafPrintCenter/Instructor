3ad4bcc881164bf6fb61319f13c762a65fd9dd85
steveasterafovo <steveasterafovo@gmail.com>
2026-08-04 17:39:34 +0100

    s

--- src/start.ts
+++ src/start.ts
@@ -13,6 +13,7 @@ const errorMiddleware = createMiddleware().server(async ({ next }) => {
     if (error != null && typeof error === "object" && "statusCode" in error) {
       throw error;
     }
+
     console.error(error);
     return new Response(renderErrorPage(), {
       status: 500,
