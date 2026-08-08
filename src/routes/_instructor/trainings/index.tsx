0c16c7a8911f77950be6ef21818d4236a91d37e2
steveasterafovo <steveasterafovo@gmail.com>
2026-08-08 17:36:23 +0100

    z

--- src/routes/_instructor/trainings/index.tsx
+++ src/routes/_instructor/trainings/index.tsx
@@ -60,7 +60,7 @@ function TrainingsPage() {
                 </div>
                 <div className="flex gap-2">
                   <Button asChild variant="accent" size="sm"><Link to="/trainings/$trainingId" params={{ trainingId: t.id }}>Ouvrir</Link></Button>
-                  <Button asChild variant="soft" size="sm"><Link to="/formations/$trainingId/contenu" params={{ trainingId: t.id }}>Contenu</Link></Button>
+                  <Button asChild variant="soft" size="sm"><Link to="/trainings/$trainingId/contenu" params={{ trainingId: t.id }}>Contenu</Link></Button>
                 </div>
               </CardContent>
             </Card>
