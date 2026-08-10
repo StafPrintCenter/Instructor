0094118cc20546cf6a42505949602420c019ed5b
steveasterafovo <steveasterafovo@gmail.com>
2026-08-10 12:35:39 +0100

    s

--- src/data/trainings.ts
+++ src/data/trainings.ts
@@ -3,7 +3,6 @@ export type TrainingLevel = "Débutant" | "Intermédiaire" | "Avancé";
 export type TrainingStatus = "draft" | "published" | "archived";
 
 export interface APIInstructorTrainingAssignment {
-  id: string; // ⚠️ à confirmer côté back — supposé égal à trainingId
   assignmentId: string;
   trainingId: string;
   trainingTitle: string;
