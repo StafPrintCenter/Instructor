2be080e34289a13d4b49469bf01886d1f4ee423e
steveasterafovo <steveasterafovo@gmail.com>
2026-08-10 13:33:45 +0100

    s

--- src/routes/_instructor/trainings/index.tsx
+++ src/routes/_instructor/trainings/index.tsx
@@ -1,3 +1,4 @@
+// src/routes/_instructor/trainings/index.tsx
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { BookOpen, MapPin, Users } from "lucide-react";
 import { PageHeader, EmptyState } from "@/components/instructor/page-header";
