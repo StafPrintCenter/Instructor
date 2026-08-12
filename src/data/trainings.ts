b24b0c2bd8a89a54f4798055e643a4b6de22b919
steveasterafovo <steveasterafovo@gmail.com>
2026-08-12 01:59:17 +0100

    d

--- src/data/trainings.ts
+++ src/data/trainings.ts
@@ -1,5 +1,3 @@
-// src/data/trainings.ts
-
 export type InstructorTrainingRole = "lead" | "assistant";
 export type TrainingLevel = "Débutant" | "Intermédiaire" | "Avancé";
 export type TrainingStatus = "draft" | "published" | "archived";
