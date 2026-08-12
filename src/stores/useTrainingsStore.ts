44f758ed5cb7aa913669def2df8a350443a440e3
steveasterafovo <steveasterafovo@gmail.com>
2026-08-12 02:05:09 +0100

    s

--- src/stores/useTrainingsStore.ts
+++ src/stores/useTrainingsStore.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { adminFetch } from "@/lib/api-url";
+import { instructorFetch } from "@/lib/api-url";
 import { createResourceStore } from "./createResourceStore";
 import type { APIInstructorTrainingAssignment, APIInstructorTrainingOverview } from "@/data/trainings";
 
