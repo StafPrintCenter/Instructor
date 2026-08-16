68293d1cb9507da986c82e8584e4f62b93b5d8ef
steveasterafovo <steveasterafovo@gmail.com>
2026-08-16 21:29:24 +0100

    d

--- src/components/site/InstructorShell.tsx
+++ src/components/site/InstructorShell.tsx
@@ -7,7 +7,7 @@ import { toast } from "sonner";
 import logo from "@/assets/logos.json";
 import { SITE } from "@/data/site";
 import { ConfirmDisconnect } from "@/components/site/InstructorBits";
-import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarProvider, SidebarTrigger, SidebarInset, } from "@/components/ui/sidebar";
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarProvider, SidebarTrigger, SidebarInset, useSidebar } from "@/components/ui/sidebar";
 
 type NavItem = {
   to: string;
