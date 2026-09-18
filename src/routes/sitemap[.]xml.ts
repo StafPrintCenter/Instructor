4b4e8a7cb3e01f8d3cf8d34614c499e0e67be0a8
steveasterafovo <steveasterafovo@gmail.com>
2026-09-18 11:08:57 +0100

    s

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -6,6 +6,7 @@ const TODAY = new Date().toISOString().split("T")[0];
 
 interface SitemapEntry {
   path: string;
+  lastmod?: string;
   changefreq?: "always" | "hourly" | "daily" | "weekly" | "monthly" | "yearly" | "never";
   priority?: string;
 }
